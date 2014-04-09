5d1b2919686bc837f0e1418b0f26c8466ade1b05
Samuel Q Singer <sqsinger1123@gmail.com>
2014-04-09 09:16:04 -0400

    new theme

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,10 @@
 		<div id="home" class="page">
 			<div id="home_top" class="span12">
-	            <h2 class="headline">Family Fun and Bakery Treats</h2>
-	            <a href="#contact">
-		            <div id="appdemo" class="img-w-caption">
-			            <img src="../img/dino_cupcakes.jpg" alt="Dinosaur Cupcakes" />
-			            <div class="caption">
-			          	    <h3>Dinosaur Cupcakes, Fresh from our Bakery!</h3>
-			            </div>
-			        </div>
-			    </a>
+	            <h2 class="headline">Welcome to Bake and Play!</h2>
+	            <div id="home_main" class="img-w-caption">
+		            We provide a new kind of bakery experience: a fun place to play, as well
+		            as a great place for yummy treats. Come on in and find out more!
+		        </div>
 	        </div> 
 	        <div id="home_lower" class="container">
 			</div>
