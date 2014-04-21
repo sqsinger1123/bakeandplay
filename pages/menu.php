ef3af9f15f127156f5381d88ef2f57aa23eebe5d
sqsinger <sqsinger1123@gmail.com>
2014-04-21 12:14:00 -0400

    menu included

--- pages/menu.php
+++ pages/menu.php
@@ -2,7 +2,219 @@
           <div id="menu_main">
             <div id="menu_description">
               <p>
-              	Full menu coming soon!
+              	
+              	<p>
+    Our bakery items are made with Natural grass fed dairy and eggs and traditional flours and sugars. No artificial dyes used for coloration or decoration.
+    All candy decorations are natural.
+</p>
+<p>
+    <strong>Cupcakes</strong>
+</p>
+<p>
+    Prices:
+</p>
+<p>
+    Individual cupcake: $2.75
+</p>
+<p>
+    Half dozen: $15.00
+</p>
+<p>
+    Dozen: $30.00
+</p>
+<p>
+    <strong>Daily Year Round</strong>
+    :
+</p>
+<p>
+    “<strong>Mysterious Dr. Play</strong>”: Bake and Play's signature cupcake<strong>; </strong>Chocolate cake with “Nutella” buttercream frosting topped with
+    mini chocolate chips<strong></strong>
+</p>
+<p>
+    <strong>“Cookies and Cream”</strong>
+    : Vanilla Oreo cake frosted with white chocolate cream cheese buttercream, topped with Oreo crumbs and Oreo piece<strong></strong>
+</p>
+<p>
+    <strong>“Sweetie-pie”</strong>
+    : Vanilla white cake with vanilla buttercream topped with sprinkles<strong></strong>
+</p>
+<p>
+    <strong>“Twister”:</strong>
+    Yellow buttermilk cake with chocolate frosting or Chocolate cake with Vanilla frosting
+</p>
+<p>
+    <strong>*Seasonal Specials</strong>
+    :<strong></strong>
+</p>
+<p>
+    <strong>Winter: Dec-Feb</strong>
+</p>
+<p>
+    <strong>“Snowball”: </strong>
+    Coconut cake with vanilla and coconut buttercream topped with flaked coconut
+</p>
+<p>
+    <strong>“Chocolate Chip</strong>
+    ”: Chocolate chip vanilla cake with white or dark chocolate frosting topped with more mini chocolate chips
+</p>
+<p>
+    <strong>“Snickerdoodle”:</strong>
+    Cinnamon cake with Cinnamon vanilla frosting topped with cinnamon sugar
+</p>
+<p>
+    <strong>“Peppermint Patty”:</strong>
+    chocolate cake with peppermint buttercream, topped with crushed stripped peppermints<strong></strong>
+</p>
+<p>
+    <strong>“Be Mine”: </strong>
+    Red velvet cake topped with cream cheese frosting edged in red sugar
+</p>
+<p>
+    <strong>“Strawberries and Chocolate”:</strong>
+    Chocolate cake frosted with Strawberry buttercream, topped with a chocolate covered strawberry
+</p>
+<p>
+    <strong>Spring: Mar-May</strong>
+</p>
+<p>
+    <strong>“Lucky Irish”: </strong>
+    Brown Sugar pound cake with Irish Cream frosting
+</p>
+<p>
+    <strong> “Cadbury”: </strong>
+    Chocolate cake filled with salted caramel, frosted with caramel buttercream topped with a caramel drizzle
+</p>
+<p>
+    <strong>“April Showers”: </strong>
+    White chocolate cream cake with raspberry white chocolate buttercream topped with white chocolate chips
+</p>
+<p>
+    <strong>“Banana Split</strong>
+    ”: Banana cake topped with banana and vanilla infused butter cream and drizzled with chocolate glaze and topped with a cherry.<strong></strong>
+</p>
+<p>
+    <strong>“Hummingbird”</strong>
+    : A hummingbird cake full of pineapple, banana, and walnuts topped with vanilla buttercream
+</p>
+<p>
+    <strong>“Strawberry Shortcake”</strong>
+    : A vanilla cake with fresh strawberries baked in, frosted with a strawberry preserved infused buttercream.<strong></strong>
+</p>
+<p>
+    <strong></strong>
+</p>
+<p>
+    <strong></strong>
+</p>
+<p>
+    <strong>Summer: Jun-Aug</strong>
+</p>
+<p>
+    <strong>“Blueberries and Cream”: </strong>
+    Vanilla cake with whole blueberries baked in topped with a blueberry buttercream
+</p>
+<p>
+    “<strong>Funfetti”</strong>: Vanilla rainbow speckled cake with vanilla buttercream and topped with sprinkles and candies
+</p>
+<p>
+    <strong>“Red, White, and Blue”: </strong>
+    Vanilla cake with mixed berries with a mixed berry buttercream
+</p>
+<p>
+    <strong>“Lemonade”: </strong>
+    Lemon citrus cake with a lemon frosting<strong></strong>
+</p>
+<p>
+    <strong>“Beach Time”: </strong>
+    Pineapple cake with pineapple glaze drizzle
+</p>
+<p>
+    <strong>“Creamcicle</strong>
+    ”: Orange cake with vanilla orange buttercream.
+</p>
+<p>
+    <strong></strong>
+</p>
+<p>
+    <strong>Fall: Sep-Nov</strong>
+</p>
+<p>
+    <strong>“PB &amp; J”: </strong>
+    Peanut butter cake filled with peanut butter cream topped with strawberry or raspberry jam flavored frosting
+</p>
+<p>
+    <strong>“Twinkie's Best Friend”:</strong>
+    Yellow buttermilk cake filled and topped with vanilla marshmallow frosting
+</p>
+<p>
+    <strong>“Maple”: </strong>
+    Maple cake with maple frosting
+</p>
+<p>
+    <strong>“The Great Pumpkin</strong>
+    ”: Pumpkin spice cake with brown butter frosting
+</p>
+<p>
+    <strong> “Bug's Bunny</strong>
+    ”: A carrot spice cake with cream cheese frosting topped with candied walnuts
+</p>
+<p>
+    <strong>“Gingerbread Man”: </strong>
+    A ginger and molasses cake with vanilla-cinnamon buttercream<strong></strong>
+</p>
+<p>
+    .
+</p>
+<p>
+    <strong>Cookies and Brownies</strong>
+</p>
+<p>
+    Prices:
+</p>
+<p>
+    Individual: $1.50
+</p>
+<p>
+    Half dozen: $8.00
+</p>
+<p>
+    Dozen: $16.00
+</p>
+<p>
+    <strong>Cookies-Daily</strong>
+</p>
+<p>
+    Chocolate chip
+</p>
+<p>
+    Soft Sugar
+</p>
+<p>
+    Oatmeal raisin
+</p>
+<p>
+    <strong>*Seasonal Specials</strong>
+    : Snickerdoodle, Peanut butter, thumbprint, double chocolate, white chocolate macadamia, pumpkin, gingerbread, maple.
+</p>
+<p>
+    <strong>Brownies-Daily</strong>
+    :
+</p>
+<p>
+    Plain chocolate
+</p>
+<p>
+    Chocolate with fudge frosting
+</p>
+<p>
+    <strong>*Seasonal Specials</strong>
+    : cheesecake brownies, caramel brownies, white chocolate blondies
+</p>
+<p>
+    Please give 48 hours for any special request orders.
+</p>
+
+
               </p>  
             </div>
           </div>
